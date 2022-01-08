ec0836fd74af1e0a8abaef9941b8618e28f5e5a5
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-08 18:48:31 -0500

    add four routes / fix delete route

--- controllers/cheeses.js
+++ controllers/cheeses.js
@@ -14,5 +14,43 @@ cheeseRouter.get("/", (req, res) => {
   });
 });
 
+delete route;
+cheeseRouter.delete("/:id", (req, res) => {
+  Cheese.findByIdAndDelete(req.params.id, (error, deletedCheese) => {
+    res.json(deletedCheese);
+  });
+});
+
+// delete route
+// cheeseRouter.delete("/:id", async (req, res) => {
+//   try {
+//     res.json(await Cheese.findByIdAndRemove(req.params.id));
+//   } catch (error) {
+//     console.log(error);
+//     res.status(404).json(error);
+//   }
+// });
+
+// update route
+cheeseRouter.put("/:id", async (req, res) => {
+  await Cheese.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (error, updatedCheese) => {
+      res.json(updatedCheese);
+    }
+  );
+});
+
+// create route
+cheeseRouter.post("/", (req, res) => {
+  Cheese.create(req.body, (error, createdCheese) => {
+    res.json(createdCheese);
+  });
+});
+
+// show route
+
 // Export cheeseRouter
 module.exports = cheeseRouter;
